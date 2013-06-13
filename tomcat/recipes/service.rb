b6cee9a691efe5e8d21662d43901d9748d4c42bb
Udo Juettner <udoj@amazon.com>
2013-06-13 16:53:29 +0200

    Fix: tomcat6 package on Ubuntu does not supply an upstart control script.

--- tomcat/recipes/service.rb
+++ tomcat/recipes/service.rb
@@ -1,10 +1,6 @@
 service 'tomcat' do
   service_name "tomcat#{node['tomcat']['base_version']}"
 
-  if platform?('ubuntu') && node[:platform_version].to_f
-    provider Chef::Provider::Service::Upstart
-  end
-
   case node[:platform]
   when 'centos','redhat','fedora','amazon'
     supports :restart => true, :reload => true, :status => true
