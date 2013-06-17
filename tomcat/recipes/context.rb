392a0bef15994899fb4ad2c202f02642d3db0cef
Udo Juettner <udoj@amazon.com>
2013-06-17 17:03:02 +0200

    Try whether generating the context works without restarting Tomcat.

--- tomcat/recipes/context.rb
+++ tomcat/recipes/context.rb
@@ -1,5 +1,3 @@
-include_recipe 'tomcat::service'
-
 node[:deploy].each do |application, deploy|
   template "context file for #{application}" do
     path ::File.join(node['tomcat']['catalina_base_dir'], 'Catalina', 'localhost', "#{application}.xml")
@@ -10,6 +8,5 @@ node[:deploy].each do |application, deploy|
     backup false
     only_if { node['datasources'][application] }
     variables(:resource_name => node['datasources'][application], :webapp_name => application)
-    notifies :restart, resources(:service => 'tomcat')
   end
 end
