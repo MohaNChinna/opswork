aa6106abac9546db66301d2454a703b13b4694bf
Udo Juettner <udoj@amazon.com>
2013-06-17 12:37:15 +0200

    Add the webapps base dir to default attributes.

--- tomcat/attributes/default.rb
+++ tomcat/attributes/default.rb
@@ -3,6 +3,7 @@ default['tomcat']['port'] = 8080
 default['tomcat']['ajp_port'] = 8009
 default['tomcat']['java_options'] = platform?('ubuntu') ? '-Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC' : ''
 default['tomcat']['catalina_base_dir'] = "/etc/tomcat#{node['tomcat']['base_version']}"
+default['tomcat']['webapps_base_dir'] = '/var/lib/tomcat6/webapps'
 default['tomcat']['lib_dir'] = "/usr/share/tomcat#{node['tomcat']['base_version']}/lib"
 default['tomcat']['java_dir'] = '/usr/share/java'
 default['tomcat']['mysql_connector_jar'] = 'mysql-connector-java.jar'
