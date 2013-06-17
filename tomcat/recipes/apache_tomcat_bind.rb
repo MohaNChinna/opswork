cd075af59dfa8641b98565e2edcbc6176945dbef
Udo Juettner <udoj@amazon.com>
2013-06-17 15:28:33 +0200

    Fix syntax errors.

--- tomcat/recipes/apache_tomcat_bind.rb
+++ tomcat/recipes/apache_tomcat_bind.rb
@@ -1,7 +1,7 @@
 execute 'enable mod_proxy for apache-tomcat binding' do
   command '/usr/sbin/a2enmod proxy'
   not_if do
-    ::File.symlink?(::File.join(node['apache']['dir']}, 'mods-enabled', 'proxy.load')) || node['tomcat']['apache_tomcat_bind_mod'] !~ /\Aproxy/
+    ::File.symlink?(::File.join(node['apache']['dir'], 'mods-enabled', 'proxy.load')) || node['tomcat']['apache_tomcat_bind_mod'] !~ /\Aproxy/
   end
 end
 
@@ -14,7 +14,7 @@ include_recipe 'tomcat::service'
 include_recipe 'apache2::service'
 
 template 'tomcat thru apache binding' do
-  path ::File.join(node['apache']['dir']}, 'conf.d', node['tomcat']['apache_tomcat_bind_config'])
+  path ::File.join(node['apache']['dir'], 'conf.d', node['tomcat']['apache_tomcat_bind_config'])
   source 'apache_tomcat_bind.conf.erb'
   owner 'root'
   group 'root'
