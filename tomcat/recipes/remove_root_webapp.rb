bc98455bde77719c6c6b4ef01f02477f194c25de
Udo Juettner <udoj@amazon.com>
2013-06-19 17:10:04 +0200

    Do not remove the ROOT context file explicitly.

--- tomcat/recipes/remove_root_webapp.rb
+++ tomcat/recipes/remove_root_webapp.rb
@@ -1,10 +1,3 @@
-ruby_block 'remove the ROOT context file' do
-  block do
-    ::File.delete(::File.join(node['tomcat']['catalina_base_dir'], 'Catalina', 'localhost', 'ROOT.xml'))
-  end
-  only_if { ::File.file?(::File.join(node['tomcat']['catalina_base_dir'], 'Catalina', 'localhost', 'ROOT.xml')) }
-end
-
 ruby_block 'remove the ROOT webapp' do
   block do
     ::FileUtils.rm_rf(::File.join(node['tomcat']['webapps_base_dir'], 'ROOT'), :secure => true)
