b49e4382e898b72ae935c0a106f037977e29388d
Udo Juettner <udoj@amazon.com>
2013-06-19 10:18:05 +0200

    Use the document root, if it's set.

--- tomcat/recipes/context.rb
+++ tomcat/recipes/context.rb
@@ -1,12 +1,17 @@
+include_recipe 'tomcat::service'
+
 node[:deploy].each do |application, deploy|
-  template "context file for #{application}" do
-    path ::File.join(node['tomcat']['catalina_base_dir'], 'Catalina', 'localhost', "#{application}.xml")
+  context_name = deploy[:mounted_at].blank? ? application : deploy[:mounted_at]
+
+  template "context file for #{application} (context name: #{context_name})" do
+    path ::File.join(node['tomcat']['catalina_base_dir'], 'Catalina', 'localhost', "#{context_name}.xml")
     source 'webapp_context.xml.erb'
     owner node['tomcat']['user']
     group node['tomcat']['group']
     mode 0640
     backup false
-    only_if { node['datasources'][application] }
-    variables(:resource_name => node['datasources'][application], :webapp_name => application)
+    only_if { node['datasources'][context_name] }
+    variables(:resource_name => node['datasources'][context_name], :webapp_name => application)
+    notifies :restart, resources(:service => 'tomcat')
   end
 end
