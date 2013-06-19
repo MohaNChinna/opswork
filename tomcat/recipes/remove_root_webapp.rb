c36d2fe701e4b4e28852c97473367e6562fea86a
Udo Juettner <udoj@amazon.com>
2013-06-19 11:24:39 +0200

    Fix condition for deletion.

--- tomcat/recipes/remove_root_webapp.rb
+++ tomcat/recipes/remove_root_webapp.rb
@@ -7,7 +7,8 @@ end
 
 ruby_block 'remove the ROOT webapp' do
   block do
-    ::FileUtils.rm_rf(::File.join(node['tomcat']['webapps_base_dir'], 'ROOT'))
+    ::FileUtils.rm_rf(::File.join(node['tomcat']['webapps_base_dir'], 'ROOT'), :secure => true)
   end
   only_if { ::File.exists?(::File.join(node['tomcat']['webapps_base_dir'], 'ROOT')) }
+  not_if { ::File.symlink?(::File.join(node['tomcat']['webapps_base_dir'], 'ROOT')) }
 end
