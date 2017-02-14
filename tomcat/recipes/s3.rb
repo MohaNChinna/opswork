cf8d19d16683ccf27d0f268f21b0ae90c09c5492
KarthicRathinam <karthic.rathan@gmail.com>
2017-02-14 19:13:39 +0530

    Add files via upload

---
+++ tomcat/recipes/s3.rb
@@ -0,0 +1,10 @@
+s3_file "/tmp/weather_reporting.war"
+  bucket "icp-cf-test"
+  remote_path "weather_reporting.war"
+  mode '0644'
+  action :create
+  aws_access_key_id node[:custom_access_key]
+  aws_secret_access_key node[:custom_secret_key]
+  decryption_key "my SHA256 digest key"
+  decrypted_file_checksum "SHA256 hex digest of decrypted file"
+end
