b9bcfc40c6814c03696ddca2f40f59606ef65bc9
MohaNChinna <mohan.solaris0606@gmail.com>
2017-02-15 16:48:10 +0530

    Update s3.rb

--- tomcat/recipes/s3.rb
+++ tomcat/recipes/s3.rb
@@ -5,6 +5,4 @@ s3_file "/tmp/weather_reporting.war" do
   action :create
   aws_access_key_id node[:custom_access_key]
   aws_secret_access_key node[:custom_secret_key]
-  decryption_key "my SHA256 digest key"
-  decrypted_file_checksum "SHA256 hex digest of decrypted file"
 end
